cba888989180fdb056da3798b4958e8e61d9cd5e
justinvo277 <truongvdnse161235@fpt.edu.vn>
2024-06-11 22:35:32 +0700

    add: early_stopping & save weight - debug: train & test

--- train.py
+++ train.py
@@ -8,18 +8,19 @@ from torch.utils.data import DataLoader
 from dataloader import TransformerDataset
 from transformer_model import TimeSeriesTransformer
 from preprocessing_data.utils import format_Dataframes, preprocessing_dataframe, split_data
-from utils import get_indices_entire_sequence, generate_square_subsequent_mask, train_loop, validation_loop
+from utils import get_indices_entire_sequence, generate_square_subsequent_mask, train_loop, validation_loop, EarlyStopping
 
 parser = argparse.ArgumentParser(description="Config")
 parser.add_argument("--data_path", type=str, help="Path of dataset", default="D:\-DSP391m-Forecasting-Financial-Time-Series-With-Transformer\craw_data\FPT_stock.xlsx")
 parser.add_argument("--log_path", type=str, help="save log for training", default=r"D:\-DSP391m-Forecasting-Financial-Time-Series-With-Transformer\log")
+parser.add_argument("--checkpoint", type=str, help="folder to save checkpoint", default=r"D:\-DSP391m-Forecasting-Financial-Time-Series-With-Transformer\checkpoint")
 parser.add_argument("--datafile_type", type=str, help="csv, xlsx, ....", default="xlsx")
 parser.add_argument("--num_rows", type=int, help="Rows of test dataset", default=720)
 
 parser.add_argument("--batch_size", type=float, help="Batch size of dataset train, test and val", default=4)
 parser.add_argument("--learning_rate", type=float, help="Learning rate for training model", default=1e-5)
 parser.add_argument("--batch_first", type=bool, help="Type batch size of dataloader", default=True)
-parser.add_argument("--max_viz", type=int, help="After n epochs will validation model", default=5)
+parser.add_argument("--max_viz", type=int, help="After n epochs will validation model", default=1)
 parser.add_argument("--epochs", type=int, help="Epochs", default=300)
 args = parser.parse_args()
 
@@ -42,19 +43,19 @@ if __name__ == "__main__":
     dataset_test = np.array(dataset_test)
 
     #Dataloader;
-    indices_data =get_indices_entire_sequence(data=dataset_train, window_size=35, step_size=1)
-
+    indices_data_train =get_indices_entire_sequence(data=dataset_train, window_size=35, step_size=1)
     dataloader_train = TransformerDataset(
         data=dataset_train,
-        indices= indices_data,
+        indices= indices_data_train,
         enc_seq_len= 30,
         dec_seq_len= 5,
         target_seq_len= 5)
     train_data = DataLoader(dataset=dataloader_train, batch_size=args.batch_size)
 
+    indices_data_test =get_indices_entire_sequence(data=dataset_test, window_size=35, step_size=1)
     dataloader_test = TransformerDataset(
-        data=dataset_train,
-        indices= indices_data,
+        data=dataset_test,
+        indices= indices_data_test,
         enc_seq_len= 30,
         dec_seq_len= 5,
         target_seq_len= 5)
@@ -88,16 +89,31 @@ if __name__ == "__main__":
     criterion_list = [('mse', mse_loss), ("mae", mae_loss), ("huber", huber_loss)]
 
 
+    #Earlt stopping;
+    early_stopping = EarlyStopping(patience=10, verbose=True, path=args.checkpoint)
+
     #Training model;
     for epoch in range(args.epochs):
 
         print(f"[{epoch+1}/{args.epochs}: TRAINING PHASE]")
         loss = train_loop(model=model, datatrain=train_data, opt=opt, criterion=mse_loss, 
         epoch=epoch, path_log=args.log_path, src_mask=src_mask, tgt_mask=tgt_mask, device=DEVICE)
-        print(f"[{epoch}/{args.epochs}: TRAINING PHASE]: MSE: {loss['MSE']}")
+        print(f"[{epoch+1}/{args.epochs}: TRAINING PHASE]: MSE: {loss['MSE']}")
 
         if epoch % args.max_viz == 0:
             print(f"[{epoch+1}/{args.epochs}: VALIDATION PHASE]")
             loss_dict = validation_loop(model=model, datatrain=test_data, criterion_list=criterion_list, 
             src_mask=src_mask, tgt_mask=tgt_mask, device=DEVICE)
-            print(f"[{epoch}/{args.epochs}: VALIDATION PHASE]: MSE: {loss_dict['mse']} | MAE: {loss_dict['mae']} | HUBER: {loss_dict['huber']}")
+            val_loss = loss_dict['mse']
+            print(f"[{epoch+1}/{args.epochs}: VALIDATION PHASE]: MSE: {loss_dict['mse']} | MAE: {loss_dict['mae']} | HUBER: {loss_dict['huber']}")
+            early_stopping(val_loss, model)
+
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -291,8 +291,45 @@ def to_numeric_and_downcast_data(df: pd.DataFrame):
 
     return df
 
+class EarlyStopping:
+    def __init__(self, patience:int=7, verbose:bool=False, delta:int=0, path:str='checkpoint.pt'):
+        """
+        EarlyStopping class giúp dừng sớm việc huấn luyện mô hình nếu validation loss không giảm.
+        Arguments:
+        patience (int): Số epoch phải chờ nếu validation loss không giảm trước khi dừng.
+        verbose (bool): Nếu True, in ra thông tin về quá trình lưu mô hình.
+        delta (float): Sự thay đổi tối thiểu để validation loss được coi là đã cải thiện.
+        path (str): Đường dẫn để lưu checkpoint mô hình.
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.delta = delta
+        self.path = path
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
 
-# if __name__ == "__main__":
-#     read_data = read_data(data_dir="D:\Major8\-DSP391m-Forecasting-Financial-Time-Series-With-Transformer\craw_data\data_csv",
-#                           timestamp_col_name="Ngày")
-#     print(read_data)
+    def __call__(self, val_loss, model):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        path_save = os.path.join(self.path, "best.pth")
+        torch.save(model.state_dict(), path_save)
+        self.val_loss_min = val_loss
